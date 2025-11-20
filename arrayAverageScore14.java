c15e4b19be4ca40b44039235f302adf7baa6ab49
onlyyourza <paseoking456@gmail.com>
2025-11-20 22:55:02 +0700

    added counting logic to track how many students passed, and then printed the result along with the average score.

--- arrayAverageScore14.java
+++ arrayAverageScore14.java
@@ -2,27 +2,42 @@ import java.util.Scanner;
 public class arrayAverageScore14 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] score = new int[10];
-        double total = 0;
-        double average;
-        int passed = 0;
+        System.out.print("Enter the number of students: ");
+        int n = sc.nextInt();
+        int[] score = new int[n];
+        double totalPassed = 0;
+        double totalFailed = 0;
+        int countPassed = 0;
+        int countFailed = 0;
 
-        for (int i = 0; i < score.length; i++) {
-            System.out.print("Enter student score " + (i + 1) + ": ");
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter the final score " + i + ": ");
             score[i] = sc.nextInt();
         }
 
-        for (int i = 0; i < score.length; i++) {
-            total += score[i];
-
+        for (int i = 0; i < n; i++) {
             if (score[i] > 70) {
-                passed++;
+                totalPassed += score[i];
+                countPassed++;
+            } else {
+                totalFailed += score[i];
+                countFailed++;
             }
         }
 
-        average = total / score.length;
-        System.out.println("The average score is: " + average);
-        System.out.println("Number of students who passed (>70): " + passed);
+        if (countPassed > 0) {
+            System.out.println("The average score of students who passed is "
+                    + (totalPassed / countPassed));
+        } else {
+            System.out.println("No students passed.");
+        }
+
+        if (countFailed > 0) {
+            System.out.println("The average score of students who failed is "
+                    + (totalFailed / countFailed));
+        } else {
+            System.out.println("No students failed.");
+        }
         sc.close();
     }
 }
