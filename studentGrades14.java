81f849ef96a800088f67308d7205a0e5c5000d72
onlyyourza <paseoking456@gmail.com>
2025-11-20 23:10:58 +0700

    create a new file Student Grade

---
+++ studentGrades14.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+public class studentGrades14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of student grades: ");
+        int n = sc.nextInt();
+
+        int[] grades = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter grade for student " + (i + 1) + ": ");
+            grades[i] = sc.nextInt();
+        }
+
+        double total = 0;
+        for (int i = 0; i < n; i++) {
+            total += grades[i];
+        }
+        double average = total / n;
+
+        int highest = grades[0];
+        int lowest = grades[0];
+
+        for (int i = 1; i < n; i++) {
+            if (grades[i] > highest) {
+                highest = grades[i];
+            }
+            if (grades[i] < lowest) {
+                lowest = grades[i];
+            }
+        }
+
+        System.out.println("\n=== Grade Summary ===");
+        System.out.println("All grades entered:");
+        for (int i = 0; i < n; i++) {
+            System.out.println("Student " + (i + 1) + ": " + grades[i]);
+        }
+
+        System.out.println("\nAverage grade: " + average);
+        System.out.println("Highest grade: " + highest);
+        System.out.println("Lowest grade: " + lowest);
+
+        sc.close();
+    }
+}
