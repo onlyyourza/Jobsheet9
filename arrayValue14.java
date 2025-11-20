1aa343b3801787660fd565935025082c3ea51239
onlyyourza <paseoking456@gmail.com>
2025-11-20 22:38:59 +0700

    change the input of final score using array lengeth and display with array length. add a condition if the student score above 70 then passed

--- arrayValue14.java
+++ arrayValue14.java
@@ -4,13 +4,15 @@ public class arrayValue14 {
         Scanner sc = new Scanner(System.in);
         int[] finalScore = new int[10];
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < finalScore.length; i++) {
             System.out.print("Enter final score for student " + i + ":  ");
             finalScore[i] = sc.nextInt();
         }
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Final score " + i + " is " + finalScore[i]);
+        for (int i = 0; i < finalScore.length; i++) {
+            if (finalScore[i] > 70) {
+                System.out.println("Student " + i + " Passed!");
+            }
         }
         sc.close();
     }
