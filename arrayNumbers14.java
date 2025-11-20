9ee82228219c6ec8300d18aaa288a6edbcd2420f
onlyyourza <paseoking456@gmail.com>
2025-11-20 22:22:26 +0700

    change the display number to for loop

--- arrayNumbers14.java
+++ arrayNumbers14.java
@@ -3,9 +3,8 @@ public class arrayNumbers14 {
 
         int[] num = {5, 12, 7, 20};
 
-        System.out.println(num[0]);
-        System.out.println(num[1]);
-        System.out.println(num[2]);
-        System.out.println(num[3]);
+        for (int i = 0; i < 4; i++) {
+            System.out.println(num[i]);
+        }
     }
 }
