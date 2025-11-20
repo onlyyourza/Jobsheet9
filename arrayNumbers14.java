2f4116849eb74cd2be428ce3e2e26229fee36154
onlyyourza <paseoking456@gmail.com>
2025-11-20 22:20:15 +0700

    2.	Modify the program code by initializing the array elements at the same time when declaring the array.

--- arrayNumbers14.java
+++ arrayNumbers14.java
@@ -1,11 +1,7 @@
 public class arrayNumbers14 {
     public static void main(String[] args) {
-        int [] num = new int [4];
-        
-        num[0] = 5;
-        num[1] = 12;
-        num[2] = 7;
-        num[3] = 20;
+
+        int[] num = {5, 12, 7, 20};
 
         System.out.println(num[0]);
         System.out.println(num[1]);
